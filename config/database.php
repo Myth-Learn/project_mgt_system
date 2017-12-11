bec25884d2f3547319c1b9b4f71947d6edfa375e
It'$ MYTH <It'$ MYTH>
2017-12-11 22:45:47 +0530

    update database

--- config/database.php
+++ config/database.php
@@ -41,16 +41,16 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'project_mgt_sys'),
+            'username' => env('DB_USERNAME', 'phub'),
+            'password' => env('DB_PASSWORD', 'lz@[4{%FOA+e'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => false,
+            'strict' => true,
             'engine' => null,
         ],
 
